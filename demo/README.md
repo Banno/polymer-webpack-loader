6ba7d19f372cf12c44f40203c5313ebf3823ac7e
Rob Dodson <robdodson@google.com>
2017-07-17 09:26:43 -0700

    Add demo

---
+++ demo/README.md
@@ -0,0 +1,15 @@
+# polymer-webpack-loader demo
+
+## Setup
+
+```
+npm i
+bower i
+npm start
+```
+
+## What's included
+
+This demo shows how to do a simple "Hello, World" with polymer-webpack-loader.
+It includes a basic project that defines a Polymer element which uses ES Module
+syntax to `import` a dependency from the `node_modules` directory.
---
+++ demo/webpack.config.js
@@ -0,0 +1,73 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+  // Tell Webpack which file kicks off our app.
+  entry: path.resolve(__dirname, 'src/index.js'),
+  // Tell Weback to output our bundle to ./dist/bundle.js
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  // Tell Webpack which directories to look in to resolve import statements.
+  // Normally Webpack will look in node_modules by default but since we’re overriding
+  // the property we’ll need to tell it to look there in addition to the
+  // bower_components folder.
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(__dirname, 'bower_components')
+    ]
+  },
+  // These rules tell Webpack how to process different module types.
+  // Remember, *everything* is a module in Webpack. That includes
+  // CSS, and (thanks to our loader) HTML.
+  module: {
+    rules: [
+      {
+        // If you see a file that ends in .html, send it to these loaders.
+        test: /\.html$/,
+        // This is an example of chained loaders in Webpack.
+        // Chained loaders run last to first. So it will run
+        // polymer-webpack-loader, and hand the output to
+        // babel-loader. This let's us transpile JS in our `<script>` elements.
+        use: [
+          { loader: 'babel-loader' },
+          { loader: 'polymer-webpack-loader' }
+        ]
+      },
+      {
+        // If you see a file that ends in .js, just send it to the babel-loader.
+        test: /\.js$/,
+        use: 'babel-loader',
+        // This line tells Webpack not to transpile .js files coming out of
+        // node_modules.
+        exclude: /node_modules/
+      }
+    ]
+  },
+  // Enable the Webpack dev server which will build, serve, and reload our
+  // project on changes.
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    // This plugin will generate an index.html file for us that can be used
+    // by the Webpack dev server. We can give it a template file (written in EJS)
+    // and it will handle injecting our bundle for us.
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.ejs'),
+      inject: false
+    }),
+    // This plugin will copy files over for us without transforming them.
+    // That's important because the custom-elements-es5-adapter.js MUST
+    // remain in ES2015.
+    new CopyWebpackPlugin([{
+      from: path.resolve(__dirname, 'bower_components/webcomponentsjs/*.js'),
+      to: 'bower_components/webcomponentsjs/[name].[ext]'
+    }])
+  ]
+};
