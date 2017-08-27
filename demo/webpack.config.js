13f07d7b495a62904fcc860c5923c7f6cec95a9b
Ruben Oostinga <0xR@users.noreply.github.com>
2017-08-27 14:16:30 +0200

    Comment out excluding node_modules from transpilation
    This will avoid questions about why components are not transpiled when they are imported from NPM.

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -40,9 +40,9 @@ module.exports = {
       {
         // If you see a file that ends in .js, just send it to the babel-loader.
         test: /\.js$/,
-        use: 'babel-loader',
-        // node_modules can be excluded except for polymer-webpack-loader
-        exclude: /node_modules\/(?!polymer-webpack-loader\/).*/
+        use: 'babel-loader'
+        // Optionally exclude node_modules from transpilation except for polymer-webpack-loader:
+        // exclude: /node_modules\/(?!polymer-webpack-loader\/).*/
       }
     ]
   },
